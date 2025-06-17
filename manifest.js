94337d6fdbfc98787347e1f9458300b0dfe6d849
A9675 <61083476+A9675@users.noreply.github.com>
2025-06-17 07:43:05 +0530

    Web push

---
+++ manifest.js
@@ -0,0 +1,16 @@
+{
+  "name": "Web Push Demo",
+  "short_name": "PushDemo",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "description": "Push Notification Subscription",
+  "icons": [
+    {
+      "src": "assets/icon.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ]
+}
+
