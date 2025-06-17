277887d316d5431f42ed8c1358d0375dab7759d6
A9675 <61083476+A9675@users.noreply.github.com>
2025-06-17 13:27:30 +0530

    Delete config .js

--- config .js
+++
@@ -1,3 +0,0 @@
-const VAPID_PUBLIC_KEY = "BBzWYKXH8HWGToWSHdyB5oHdLU5aFfvngcKNg8N4pSmMxmOZ00C3uEd1Evn4av9dcjz6HpBdvfPwQtm9vM3MjE0";
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx7v5mw60S9Z0QWKlQCdaVxIWdC0z4ysAkxNbtf70AycDJ2mh-hwrCpJRgXcskXz2Eb/exec";
-
