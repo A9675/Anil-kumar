e3711a9b41bbd96443dd606817eea4dd301a41f6
A9675 <61083476+A9675@users.noreply.github.com>
2025-06-17 13:30:01 +0530

    New file
    New update

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+  publicKey: "BKb2svsveoHwhffaai1_9zjFLNVPXWlNRn4QGAn6pD8_2FjwVsEJFaJ8rkJvjQ6aS8rpQk09H2SLPChl4cS0Phk",
+  sheetUrl: "https://script.google.com/macros/s/AKfycbzQIa2aOG4FjrSpiQ4d0FRg6kh-wIRBNEHPp62u8AmIxFOM2-NPCeZ05VmAhJZHntvb/exec"
+};
---
+++ subscribe.js
@@ -0,0 +1,23 @@
+async function subscribeUser() {
+  if (!('serviceWorker' in navigator)) return alert("❌ Service Worker not supported.");
+  const registration = await navigator.serviceWorker.register('sw.js');
+
+  const subscription = await registration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: urlBase64ToUint8Array(CONFIG.publicKey)
+  });
+
+  await fetch(CONFIG.sheetUrl, {
+    method: 'POST',
+    body: JSON.stringify(subscription)
+  });
+
+  alert("✅ Subscribed & sent to Google Sheet!");
+}
+
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
+  const rawData = atob(base64);
+  return Uint8Array.from([...rawData].map(char => char.charCodeAt(0)));
+}
